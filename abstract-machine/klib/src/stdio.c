774c42b132b22b48300be854d65e941386761e74
wanghao <87025569@qq.com>
2023-12-25 11:57:44 +0800

    fix klib

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,22 +5,28 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int vsprintf(char *out, const char *fmt, va_list ap);
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap);
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+	char buffer[2048];
+	va_list arg;
+	va_start (arg, fmt);
+	int done = vsprintf(buffer, fmt, arg);
+	putstr(buffer);
+	va_end(arg);
+	return done;
 }
 
-
-static char HEX_CHARACTERS[] = "0123456789ABCDEF";
+//static char HEX_CHARACTERS[] = "0123456789ABCDEF";
 #define BIT_WIDE_HEX 8
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  //panic("Not implemented");
-  return vsnprintf(out, -1, fmt, ap);
+  	return vsnprintf(out, -1, fmt, ap);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  //panic("Not implemented");
-  va_list valist;
+	va_list valist;
 	va_start(valist, fmt);
 	int res = vsprintf(out ,fmt, valist);
 	va_end(valist);
@@ -28,92 +34,91 @@ int sprintf(char *out, const char *fmt, ...) {
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list arg;
+	va_start (arg, fmt);
+	int done = vsnprintf(out, n, fmt, arg);
+	va_end(arg);
+	return done;
 }
 
 #define append(x) {out[j++]=x; if (j >= n) {break;}}
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-	char buffer[128];
-	char *txt, cha;
-	int num, len;
-	unsigned int unum;
-	uint32_t pointer;
-	int state = 0, i, j;
-	for (i = 0, j = 0; fmt[i] != '\0'; ++i){
-		switch (state){
-		/*
-		* this state is used to copy the contents of the 
-		* string to the buffer 
-		*/
-		case 0:
-			if (fmt[i] != '%'){
-				append(fmt[i]);
-			} 
-			else
-				state = 1;
-			break;
-		/*
-		* this state is used to deal with the format controler  
-		*/
-		case 1:
-			switch (fmt[i]){
-				case 's':
-					txt = va_arg(ap, char*);
-					for (int k = 0; txt[k] !='\0'; ++k)
-						append(txt[k]);
-					break;
-				
-				case 'd':
-					num = va_arg(ap, int);
-					if(num == 0){
-						append('0');
-						break;
-					}
-					if (num < 0){
-						append('-');
-						num = 0 - num;
-					}
-					for (len = 0; num ; num /= 10, ++len)
-						buffer[len] = HEX_CHARACTERS[num % 10];
-					for (int k = len - 1; k >= 0; --k)
-						append(buffer[k]);
-					break;
-				
-				case 'c':
-					cha = (char)va_arg(ap, int);
-					append(cha);
-					break;
-
-				case 'p':
-					pointer = va_arg(ap, uint32_t);
-					for (len = 0; pointer ; pointer /= 16, ++len)
-						buffer[len] = HEX_CHARACTERS[pointer % 16];
-					for (int k = 0; k < BIT_WIDE_HEX - len; ++k)
-						append('0');
-					for (int k = len - 1; k >= 0; --k)
-						append(buffer[k]);
-					break;
-				case 'x':
-					unum = va_arg(ap, unsigned int);
-					if(unum == 0){
-						append('0');
-						break;
-					}
-					for (len = 0; unum ; unum >>= 4, ++len)
-						buffer[len] = HEX_CHARACTERS[unum & 0xF];
-					for (int k = len - 1; k >= 0; --k)
-						append(buffer[k]);
-					break;  
-				default:
-					assert(0);
-			}
-		state = 0;
-		break;
-		}
-	}
-	out[j] = '\0';
-	return j;
+void reverse(char* start, char* end) {
+    while (start < end) {
+        char temp = *start;
+        *start++ = *end;
+        *end-- = temp;
+    }
 }
+
+int vsnprintf(char* str, size_t len, const char* format, va_list args) {
+    char* start = str;
+    char padding_char;
+    int min_field_width;
+
+    for (; *format != '\0'; format++) {
+        if (*format == '%') {
+            padding_char = ' ';
+            min_field_width = 0;
+
+            format++;
+            while (*format >= '0' && *format <= '9') {
+                min_field_width *= 10;
+                min_field_width += (*format - '0');
+                format++;
+            }
+            if (*format == '0') {
+                padding_char = '0';
+                format++;
+            }
+
+            min_field_width = min_field_width ? min_field_width : 1;
+
+            switch(*format) {
+                case 'd': {
+                    int value = va_arg(args, int);
+                    char number[11];
+                    char* ptr = number;
+
+                    if (value == 0) {
+                        *ptr++ = '0';
+                    } else {
+                        while (value > 0) {
+                            *ptr++ = '0' + value % 10;
+                            value /= 10;
+                        }
+                    }
+                    int num_digits = ptr - number;
+                    reverse(number, ptr - 1);
+
+                    while (num_digits < min_field_width && start - str < len - 1) {
+                        *start++ = padding_char;
+                        num_digits++;
+                    }
+
+                    for (char *p = number; p < ptr && start - str < len - 1; p++) {
+                        *start++ = *p;
+                    }
+                    break;
+                }
+                case 's': {
+                    char* ptr = va_arg(args, char*);
+                    while (*ptr != '\0' && start - str < len - 1) {
+                        *start++ = *ptr++;
+                    }
+                    break;
+                }
+                default:
+                    return -1;
+            }
+        } else if (start - str < len - 1) {
+            *start++ = *format;
+        }
+    }
+
+    *start = '\0';
+    return start - str;
+}
+
 
 #endif
